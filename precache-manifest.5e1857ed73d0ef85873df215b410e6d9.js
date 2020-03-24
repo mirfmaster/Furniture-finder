b098553fe5b44fcbf36983a9adff277da81dce35
mirfmaster <mirfmaster@gmail.com>
2020-03-24 16:15:18 +0700

    Updates

---
+++ precache-manifest.5e1857ed73d0ef85873df215b410e6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ab7ebb0658735aebd7675edaddf182",
+    "url": "/Furniture-finder/index.html"
+  },
+  {
+    "revision": "e9afeeae76c2c4d16326",
+    "url": "/Furniture-finder/static/css/main.f844d6cf.chunk.css"
+  },
+  {
+    "revision": "ff207e4f3cc1b4128b87",
+    "url": "/Furniture-finder/static/js/2.cbadc93b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Furniture-finder/static/js/2.cbadc93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9afeeae76c2c4d16326",
+    "url": "/Furniture-finder/static/js/main.8a9fbba9.chunk.js"
+  },
+  {
+    "revision": "4d4aa8350bf020e8fe3d",
+    "url": "/Furniture-finder/static/js/runtime-main.61bbd944.js"
+  }
+]);
